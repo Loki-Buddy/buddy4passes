5c1793e2a44a96536ffd8ea7176d1ce23b2cd6c6
Christof Gaßmann <christof.gassmann@tn.techstarter.de>
2025-11-04 16:48:16 +0100

    fix: userLogin Route angepasst

--- Cloud/routes/userLogin.js
+++ Cloud/routes/userLogin.js
@@ -1,29 +1,36 @@
 const express = require("express");
 const router = express.Router();
 const jwt = require("jsonwebtoken");
+const { Pool } = require("pg");
 require("dotenv").config();
 
+const pool = new Pool({
+  user: process.env.DB_USER,
+  host: process.env.DB_HOST,
+  database: process.env.DB_NAME,
+  password: process.env.DB_PASSWORD,
+  port: process.env.DB_PORT,
+  ssl: {
+    rejectUnauthorized: false,
+  },
+});
+
 router.post("/user/login", async (req, res) => {
-  const { userEmail, masterPW } = req.body;
+  const { userName, masterPW } = req.body;
 
   try {
-    const user = await pool.query(
-      "SELECT * FROM b4puser WHERE userEmail = $1",
-      [userEmail]
-    );
+    const user = await pool.query("SELECT * FROM b4puser WHERE userName = $1", [
+      userName,
+    ]);
     if (user.rows.length === 0) {
-      return res
-        .status(401)
-        .json({
-          message: "Loginversuch fehlgeschlagen, bitte überprüfe deine Eingae!",
-        });
+      return res.status(401).json({
+        message: "Loginversuch fehlgeschlagen, bitte überprüfe deine Eingabe!",
+      });
     }
     if (user.rows[0].masterPW !== masterPW) {
-      return res
-        .status(401)
-        .json({
-          message: "Loginversuch fehlgeschlagen, bitte überprüfe deine Eingae!",
-        });
+      return res.status(401).json({
+        message: "Loginversuch fehlgeschlagen, bitte überprüfe deine Eingabe!",
+      });
     }
     const token = jwt.sign(
       { userID: user.rows[0].userID },
