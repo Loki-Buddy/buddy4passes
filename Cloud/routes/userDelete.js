5494ea50751dae784b4775fd409e56be7676f1fc
Beatbroetchen88 <sebastianrehberg.rehberg@tn.techstarter.de>
2025-11-05 12:53:23 +0100

    Update userDelete.js

--- Cloud/routes/userDelete.js
+++ Cloud/routes/userDelete.js
@@ -1,36 +1,27 @@
 const express = require("express");
 const router = express.Router();
-const { Pool } = require("pg");
 require("dotenv").config();
 const auth = require("../middleware/auth"); // User muss für die Aktion eingeloggt sein
+const pool = require("../pool")
 
-const pool = new Pool({
-  user: process.env.DB_USER,
-  host: process.env.DB_HOST,
-  database: process.env.DB_NAME,
-  password: process.env.DB_PASSWORD,
-  port: process.env.DB_PORT,
-  ssl: {
-    rejectUnauthorized: false,
-  },
-});
 // Löschen-Route
 router.delete("/user/delete", auth, async (req, res) => {
-    const { userEmail }=req.body; // E-Mail oder ID sinnvoller? Einbau Token?
+    const { user_email } = req.body; // E-Mail und ID werden verwendet
+    const {user_id} = req.user
 
-    if(!userEmail) {
+    if(!user_email) {
         return res.status(400).json({message: "Bitte gib eine gültige E-Mail an."});
     }
     // Existiert der User?
     try {
         const existingUser = await pool.query(
-            "SELECT * FROM b4puser WHERE userEmail = $1", [userEmail]
+            "SELECT * FROM b4puser WHERE user_email = $1 AND user_id = $2", [user_email, user_id]
         );
         if (existingUser.rows.length===0) {
             return res.status(404).json({message:"Dieser User existiert nicht."});
         }
         // User löschen mit dependencies ON DELETE CASCADE 
-        await pool.query("DELETE FROM b4puser WHERE userEmail = $1", [userEmail]);
+        await pool.query("DELETE FROM b4puser WHERE user_email = $1", [user_email]);
 
         res.status(200).json({message: "Nutzer und zugehörige Daten wurden erfolgreich gelöscht."});
     } catch (err) {
