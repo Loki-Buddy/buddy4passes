bb6997d997264db1943d05bd4770056493bf68cf
Christof Gaßmann <christof.gassmann@tn.techstarter.de>
2025-11-05 12:26:13 +0100

    fix: displayAccount route angepasst, sodass die account_id mit zurückgegeben wird

--- Cloud/routes/displayAccounts.js
+++ Cloud/routes/displayAccounts.js
@@ -4,21 +4,19 @@ const router = express.Router();
 const pool = require("../pool");
 
 router.get("/accounts", auth, async (req, res) => {
-    try {
-        const userId = req.user.user_id;
+  try {
+    const userId = req.user.user_id;
 
-        const result = await pool.query(
-            "SELECT service, service_email, service_username, service_password FROM accounts WHERE user_id = $1",
-            [userId]
-        );
+    const result = await pool.query(
+      "SELECT account_id, service, service_email, service_username, service_password FROM accounts WHERE user_id = $1",
+      [userId]
+    );
 
-        res.json(result.rows);
-    } catch (err) {
-        console.error(err);
-        res.status(500).json({ message: "Serverfehler" });
-    }
+    res.json(result.rows);
+  } catch (err) {
+    console.error(err);
+    res.status(500).json({ message: "Serverfehler" });
+  }
 });
 
 module.exports = router;
-
-
