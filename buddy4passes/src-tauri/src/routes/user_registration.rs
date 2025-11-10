fc59a65f27255dca325bf64c8dd8210b340e97e0
Joshua Anders <joshua.anders@tn.techstarter.de>
2025-11-10 16:12:19 +0100

    User Registration Route fertiggestellt.

--- buddy4passes/src-tauri/src/routes/user_registration.rs
+++ buddy4passes/src-tauri/src/routes/user_registration.rs
@@ -1,13 +1,22 @@
 use serde::{Deserialize, Serialize};
+use std::fs;
 use tauri::State;
 use reqwest::Client;
 use std::sync::Arc;
+use std::env;
 use serde_json::Value;
 use crate::crypt::crypt::hash_password;
-use crate::crypt::crypt;
+use crate::crypt::crypt::CryptoService;
 
-let test = CryptoService::new_random();
-
+pub fn create_and_save_key(file_path: &str) -> Result<(), std::io::Error> {
+    // Erstelle neuen zufälligen CryptoService
+    let crypto = CryptoService::new_random();
+    // Hole den Schlüssel als Base64-String
+    let key_base64 = crypto.get_key();
+    // Speichere den Schlüssel in einer Datei
+    fs::write(file_path, key_base64)?;
+    Ok(())
+}
 #[derive(Clone, Deserialize, Serialize)]
 struct NewUser {
     user_name: String,
@@ -39,12 +48,17 @@ pub async fn register_user_test(
         .await
         .map_err(|e| format!("HTTP request failed: {}", e))?;
 
-
+    if resp.status().is_success() {
+        let appdata = env::var("LOCALAPPDATA")
+        .or_else(|_| env::var("USER"))
+        .unwrap_or_else(|_| "Unbekannt".to_string());
+        fs::create_dir_all(&format!("{}\\Buddy4Passes", appdata)).expect("Failed to create directory");
+        create_and_save_key(&format!("{}\\Buddy4Passes\\user_key_{}.txt", appdata, &new_user.user_name.as_str())).expect("Failed to create and save key");
+    }
     let json = resp
-        .json::<Value>()
-        .await
-        .map_err(|e| format!("Failed to parse JSON response: {}", e))?;
-
-
+    .json::<Value>()
+    .await
+    .map_err(|e| format!("Failed to parse JSON response: {}", e))?;
+    
     Ok(json)
 }
