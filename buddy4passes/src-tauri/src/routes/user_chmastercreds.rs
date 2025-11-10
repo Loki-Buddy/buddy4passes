11e8927f38d08b0d8c25b3a8a59adc07a7df5c56
Loki-Buddy <jm.conring@live.de>
2025-11-10 09:38:10 +0100

    änderungen an den Credantials implementiert

--- buddy4passes/src-tauri/src/routes/user_chmastercreds.rs
+++ buddy4passes/src-tauri/src/routes/user_chmastercreds.rs
@@ -2,32 +2,38 @@ use serde::{Serialize, Deserialize};
 use tauri::State;
 use reqwest::Client;
 use std::sync::Arc;
-use serde_json::Value;
+use serde_json::{Value, json};
 
 #[derive(Serialize, Deserialize)]
 pub struct MasterData {
-    new_user_name: String,
-    new_user_email: String,
-    old_master_password: String,
-    new_master_password: String,
-    confirm_new_master_password: String,
+    #[serde(skip_serializing_if = "Option::is_none")]
+    new_user_name: Option<String>,
+    #[serde(skip_serializing_if = "Option::is_none")]
+    new_user_email: Option<String>,
+    #[serde(skip_serializing_if = "Option::is_none")]
+    old_master_password: Option<String>,
+    #[serde(skip_serializing_if = "Option::is_none")]
+    new_master_password: Option<String>,
+    #[serde(skip_serializing_if = "Option::is_none")]
+    confirm_new_master_password: Option<String>,
 }
 
 #[tauri::command]
 pub async fn change_master_creds(client: State<'_, Arc<Client>>, data: MasterData) -> Result<Value, String> {
-    let data = MasterData {
-        new_user_name: data.new_user_name.to_string(),
-        new_user_email: data.new_user_email.to_string(),
-        old_master_password: data.old_master_password.to_string(),
-        new_master_password: data.new_master_password.to_string(),
-        confirm_new_master_password: data.confirm_new_master_password.to_string(),
-    };
-    
+    // Erstellen Sie ein neues JSON-Objekt, das nur die vorhandenen Felder enthält
+    let request_data = json!({
+        "new_user_name": data.new_user_name,
+        "new_user_email": data.new_user_email,
+        "old_master_password": data.old_master_password,
+        "new_master_password": data.new_master_password,
+        "confirm_new_master_password": data.confirm_new_master_password,
+    });
+
     let token = "";
     
     let response = client
         .put("http://3.74.73.164:3000/user/chmastercreds")
-        .json(&data)
+        .json(&request_data)
         .send()
         .await
         .map_err(|e| e.to_string())?
