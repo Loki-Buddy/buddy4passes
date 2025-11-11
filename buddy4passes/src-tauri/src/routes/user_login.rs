53d7282a5470e893faa012d4236ec07277332cfa
Beatbroetchen88 <sebastianrehberg.rehberg@tn.techstarter.de>
2025-11-11 14:49:18 +0100

    Update user_login.rs

--- buddy4passes/src-tauri/src/routes/user_login.rs
+++ buddy4passes/src-tauri/src/routes/user_login.rs
@@ -4,9 +4,9 @@ use reqwest::Client;
 use serde::{Deserialize, Serialize};
 use serde_json::Value;
 use std::sync::{Arc, Mutex};
-// use std::fs;
-// use std::env;
-// use crate::crypt::crypt::verify_password;
+use std::fs;
+use std::env;
+use crate::crypt::crypt::verify_password;
 
 #[derive(Serialize)]
 struct LoginRequest {
@@ -42,8 +42,8 @@ pub async fn login_user(
     client: State<'_, Arc<Client>>,
     state: State<'_, Arc<MemoryStore>>,
     username: String,
-    // masterpassword: String,
-) -> Result<Value, String> {
+    masterpassword: String,
+) -> Result<LoginResult, String> {
     {let stored = state.token.lock().unwrap();
         println!("Token zu Beginn der Funktion: {:?}", *stored);
     }
@@ -59,85 +59,88 @@ pub async fn login_user(
         })
         .send()
         .await
-        .map_err(|e| format!("Fehler beim Senden der Anfrage: {}", e))?
+        .map_err(|e| format!("Fehler beim Senden der Anfrage: {}", e))?;
+        
+
+
+    if !hash_response.status().is_success() {
+        return Ok(LoginResult {
+            success: false,
+            message: format!("Login fehlgeschlagen"),
+        });
+    };
+
+    let user_json = hash_response
         .json::<Value>()
+        .await
+        .map_err(|_| format!("Antwort konnte nicht gelesen werden"))?;
+    // let mut login_response: Value = hash_response
+    let old_master_password_hash = user_json["master_password"].as_str().unwrap_or_default().to_string();
+    // println!("Password: {}", old_master_password_hash);
+
+    
+    
+    // Cloud-Backend-Endpunkt
+    let api_url="http://3.74.73.164:3000/user/login";
+
+    // Anfrage absenden
+    let response = client
+        .post(api_url)
+        .json(&LoginRequest {
+            user_name: username,
+            master_password: masterpassword,
+        })
+        .send()
+        .await
+        .map_err(|e| format!("Fehler beim Senden der Anfrage: {}", e))?;
+
+    // Antwort prüfen
+    let status = response.status();
+    if !status.is_success() {
+        return Ok(LoginResult {
+            success: false,
+            message: format!("Login fehlgeschlagen (HTTP {}): {}", status.as_u16(), status),
+        });
+    }
+
+
+    let login_response: LoginResponse = response
+        .json()
         .await
         .map_err(|e| format!("Antwort konnte nicht gelesen werden: {}", e))?;
 
-    // let status = hash_response.status();
-    // if !status.is_success() {
-    //     // return Ok(LoginResult {
-    //     //     success: false,
-    //     //     message: format!("Login fehlgeschlagen (HTTP {}): {}", status.as_u16(), status),
-    //     // });
-    // };
-    // let mut login_response: Value = hash_response
-    let old_master_password_hash = hash_response["master_password"].as_str().unwrap_or_default().to_string();
-    println!("passwort: {}", old_master_password_hash);
-    return Ok(hash_response);
-
-    }
-    
-    // Cloud-Backend-Endpunkt
-//     let api_url="http://3.74.73.164:3000/user/login";
-
-//     // Anfrage absenden
-//     let response = client
-//         .post(api_url)
-//         .json(&LoginRequest {
-//             user_name: username,
-//             master_password: masterpassword,
-//         })
-//         .send()
-//         .await
-//         .map_err(|e| format!("Fehler beim Senden der Anfrage: {}", e))?;
-
-//     // Antwort prüfen
-//     let status = response.status();
-//     if !status.is_success() {
-//         return Ok(LoginResult {
-//             success: false,
-//             message: format!("Login fehlgeschlagen (HTTP {}): {}", status.as_u16(), status),
-//         });
-//     }
-
-
-//     let login_response: LoginResponse = response
-//         .json()
-//         .await
-//         .map_err(|e| format!("Antwort konnte nicht gelesen werden: {}", e))?;
-
         
 
-//     if let Some(token)=login_response.token {
-//         // Token im Speicher ablegen
-//         if let Ok(mut stored_token) = state.token.lock() {
-//             *stored_token = Some(token);
-//         } else {
-//             eprintln!("Warnung: Token konnte nicht in MemoryStore gespeichert werden!");
-//         }
-//         {let stored = state.token.lock().unwrap().clone().unwrap_or_default();
-//         println!("Token zum Ende der Funktion: {:?}", stored);
-//         }
-//         {
-//             let appdata = env::var("LOCALAPPDATA")
-//                 .or_else(|_| env::var("USER"))
-//                 .unwrap_or_else(|_| "Unbekannt".to_string());
-//             let test = fs::read(&format!("{}\\Buddy4Passes\\user_key_{}.txt", appdata, &username.as_str()));
-//             println!("key: {}", test);
-//         }
-//         {let stored = state.key.lock().unwrap().clone().unwrap_or_default();
-//         println!("Key zum Ende der Funktion: {:?}", stored);
-//         }
-//         Ok(LoginResult {
-//             success: true,
-//             message: format!("{} (Token gespeichert)", login_response.message),
-//         })
-//     } else {
-//         Ok(LoginResult {
-//             success: false,
-//             message: format!("{} (kein Token erhalten)", login_response.message),
-//         })
-//     }
-// }
+    if let Some(token)=login_response.token {
+        // Token im Speicher ablegen
+        if let Ok(mut stored_token) = state.token.lock() {
+            *stored_token = Some(token);
+        } else {
+            eprintln!("Warnung: Token konnte nicht in MemoryStore gespeichert werden!");
+        }
+        {let stored = state.token.lock().unwrap().clone().unwrap_or_default();
+        println!("Token zum Ende der Funktion: {:?}", stored);
+        }
+        {
+            let appdata = env::var("LOCALAPPDATA")
+                .or_else(|_| env::var("USER"))
+                .unwrap_or_else(|_| "Unbekannt".to_string());
+            let test = fs::read(&format!("{}\\Buddy4Passes\\user_key_{}.txt", appdata, &username.as_str())).map_err(|_| format!("Fehler beim Lesen!"));
+            let test2=String::from_utf8(test?);
+            println!("key: {}", test2);
+        }
+        {let stored = state.key.lock().unwrap().clone().unwrap_or_default();
+        println!("Key zum Ende der Funktion: {:?}", stored);
+        }
+        Ok(LoginResult {
+            success: true,
+            message: format!("{} (Token gespeichert)", login_response.message),
+        })
+    } else {
+        Ok(LoginResult {
+            success: false,
+            message: format!("{} (kein Token erhalten)", login_response.message),
+        })
+    }
+}
 
