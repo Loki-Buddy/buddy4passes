51abe8ade3bc930ba26c0b40ae99e01b34f6044c
Christof Gaßmann <christof.gassmann@tn.techstarter.de>
2025-11-12 15:40:27 +0100

    merge from main

--- buddy4passes/src/App.jsx
+++ buddy4passes/src/App.jsx
@@ -40,117 +40,118 @@ function App() {
   }
 
   async function addAccount() {
-  const data = {
-    service: "Google",
-    service_email: "Sebs@gmail.com",
-    service_username: "Sebs",
-    service_password: "PW123",
-  };
-
-  try {
-    const response = await invoke("add_account", {
-      servicename: data.service,
-      serviceemail: data.service_email,
-      serviceusername: data.service_username,
-      servicepassword: data.service_password,
-    });
-
-    console.log("Server response:", response);
-    console.log("Typ der response:", typeof response);
-    console.log("message:", response.message);
-
-    if (response.success) {
-      alert(`${response.message}`);
-    } else {
-      alert(`${response.message}`);
-    }
-  } catch (error) {
-    console.error("Fehler beim Hinzufügen des Accounts:", error);
-    alert(`Fehler: ${error}`);
-  }
-}
-
-async function registerUser() {
-  async function registerUser() {
     const data = {
-      name: "lokii",
-      email: "test@lokiiTEST.de",
-      masterpassword: "test",
+      service: "Google",
+      service_email: "Sebs@gmail.com",
+      service_username: "Sebs",
+      service_password: "PW123",
     };
 
-    const response = await invoke("register_user_test", {
-      name: data.name,
-      email: data.email,
-      masterpassword: data.masterpassword,
-    });
-    console.log("Server response:", response);
-    console.log("Typ der response:", typeof response);
-    console.log("message", response.message);
+    try {
+      const response = await invoke("add_account", {
+        servicename: data.service,
+        serviceemail: data.service_email,
+        serviceusername: data.service_username,
+        servicepassword: data.service_password,
+      });
+
+      console.log("Server response:", response);
+      console.log("Typ der response:", typeof response);
+      console.log("message:", response.message);
+
+      if (response.success) {
+        alert(`${response.message}`);
+      } else {
+        alert(`${response.message}`);
+      }
+    } catch (error) {
+      console.error("Fehler beim Hinzufügen des Accounts:", error);
+      alert(`Fehler: ${error}`);
+    }
   }
 
-  async function deleteUser() {
-    const email = "test@test.de";
-    const username = "Chris1";
-    const response = await invoke("delete_user", { email, username });
-    console.log("Server response:", response);
-    console.log("Typ der response:", typeof response);
-    console.log("message", response.message);
+  async function registerUser() {
+    async function registerUser() {
+      const data = {
+        name: "lokii",
+        email: "test@lokiiTEST.de",
+        masterpassword: "test",
+      };
+
+      const response = await invoke("register_user_test", {
+        name: data.name,
+        email: data.email,
+        masterpassword: data.masterpassword,
+      });
+      console.log("Server response:", response);
+      console.log("Typ der response:", typeof response);
+      console.log("message", response.message);
+    }
+
+    async function deleteUser() {
+      const email = "test@test.de";
+      const username = "Chris1";
+      const response = await invoke("delete_user", { email, username });
+      console.log("Server response:", response);
+      console.log("Typ der response:", typeof response);
+      console.log("message", response.message);
+    }
+    async function loginUser() {
+      const username = "Sebs1";
+      const masterpassword = "test";
+      const response = await invoke("login_user", { username, masterpassword });
+      console.log("Server response:", response);
+      console.log("Typ der response:", typeof response);
+      console.log("message", response.message);
+    }
+
+    async function getAccounts() {
+      const email = "test@test.de";
+      const username = "Chris1";
+      const response = await invoke("display_accounts");
+      console.log("Server response:", response);
+      console.log("Typ der response:", typeof response);
+      console.log("message", response.message);
+    }
+
+    return (
+      <main className="container">
+        <h1>Welcome to buddy4passes</h1>
+
+        <div className="row">
+          <a href="https://vite.dev" target="_blank">
+            <img src="/vite.svg" className="logo vite" alt="Vite logo" />
+          </a>
+          <a href="https://tauri.app" target="_blank">
+            <img src="/tauri.svg" className="logo tauri" alt="Tauri logo" />
+          </a>
+          <a href="https://react.dev" target="_blank">
+            <img src={reactLogo} className="logo react" alt="React logo" />
+          </a>
+        </div>
+        <p>Click on the Tauri, Vite, and React logos to learn more.</p>
+
+        <form
+          className="row"
+          onSubmit={(e) => {
+            e.preventDefault();
+            // deleteUser();
+            // registerUser();
+            addAccount();
+            // loginUser();
+          }}
+        >
+          <input
+            id="greet-input"
+            onChange={(e) => setName(e.currentTarget.value)}
+            placeholder="Enter a name..."
+          />
+          <button type="submit">Greet</button>
+        </form>
+        <p>{greetMsg}</p>
+      </main>
+    );
   }
-  async function loginUser() {
-    const username = "Sebs1";
-    const masterpassword = "test";
-    const response = await invoke("login_user", { username, masterpassword });
-    console.log("Server response:", response);
-    console.log("Typ der response:", typeof response);
-    console.log("message", response.message);
-  }
-
-  async function getAccounts() {
-    const email = "test@test.de";
-    const username = "Chris1";
-    const response = await invoke("display_accounts");
-    console.log("Server response:", response);
-    console.log("Typ der response:", typeof response);
-    console.log("message", response.message);
-  }
-
-  return (
-    <main className="container">
-      <h1>Welcome to buddy4passes</h1>
-
-      <div className="row">
-        <a href="https://vite.dev" target="_blank">
-          <img src="/vite.svg" className="logo vite" alt="Vite logo" />
-        </a>
-        <a href="https://tauri.app" target="_blank">
-          <img src="/tauri.svg" className="logo tauri" alt="Tauri logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <p>Click on the Tauri, Vite, and React logos to learn more.</p>
-
-      <form
-        className="row"
-        onSubmit={(e) => {
-          e.preventDefault();
-          // deleteUser();
-          // registerUser();
-          addAccount();
-          // loginUser();
-        }}
-      >
-        <input
-          id="greet-input"
-          onChange={(e) => setName(e.currentTarget.value)}
-          placeholder="Enter a name..."
-        />
-        <button type="submit">Greet</button>
-      </form>
-      <p>{greetMsg}</p>
-    </main>
-  );
 }
 
 export default App;
