fcf078812a5afee95089152246e33bcd14f1c0ea
Joshua Anders <joshua.anders@tn.techstarter.de>
2025-11-13 17:10:51 +0100

    Update LoginForm.jsx

--- buddy4passes/src/components/LoginForm.jsx
+++ buddy4passes/src/components/LoginForm.jsx
@@ -1,21 +1,18 @@
 import { useState } from "react";
 import TextField from "@mui/material/TextField";
 import Button from "@mui/material/Button";
-import { Link } from "react-router-dom";
 import { invoke } from "@tauri-apps/api/core";
 import Snackbar from "@mui/material/Snackbar";
 import { useNavigate } from "react-router-dom";
+import { Link } from "react-router-dom";
 
-function RegistrationForm() {
+function LoginForm() {
   const navigate = useNavigate();
-  const [email, setEmail] = useState("");
   const [username, setUsername] = useState("");
-  const [password, setPassword] = useState("");
-  const [confirmPassword, setConfirmPassword] = useState("");
+  const [masterpassword, setMasterpassword] = useState("");
 
-  const [emailValidationError, setEmailValidationError] = useState("");
-  const [usernameValidationError, setUsernameValidationError] = useState("");
-  const [passwordValidationError, setPasswordValidationError] = useState("");
+  const [userNameValidationError, setUsernameValidationError] = useState("");
+  const [masterPasswordValidationError, setPasswordValidationError] = useState("");
 
   const [snackbarOpen, setSnackbarOpen] = useState(false);
   const [snackbarMessage, setSnackbarMessage] = useState("");
@@ -23,117 +20,87 @@ function RegistrationForm() {
   async function handleSubmit(e) {
     e.preventDefault();
 
-    if (password !== confirmPassword) {
-      setPasswordValidationError("Passwörter stimmen nicht überein");
+    if (masterpassword === "") {
+      setPasswordValidationError("Passwort darf nicht leer sein");
       return;
     } else {
       setPasswordValidationError("");
     }
 
+    if (username === "") {
+      setUsernameValidationError("Benutzername darf nicht leer sein");
+      return;
+    } else {
+      setUsernameValidationError("");
+    }
+
     const data = {
-      email,
       username,
-      password,
+      masterpassword,
     };
     try {
-      const response = await invoke("register_user_test", {
-        name: data.username,
-        email: data.email,
-        masterpassword: data.password,
-      });
-
-      switch (response.message) {
-        case "Dieser Benutzername wird bereits von einem anderen User verwendet.":
-          setUsernameValidationError("Nutzername wird bereits verwendet");
-          return;
-        case "Diese E-Mail wird bereits von einem anderen User verwendet.":
-          setEmailValidationError("E-Mail wird bereits verwendet");
-          return;
-        default:
-          break;
+      const response = await invoke("login_user", data);
+      if (response.message === "Login erfolgreich! (Token gespeichert)") {
+        setSnackbarMessage("Login erfolgreich!");
+        setSnackbarOpen(true);
+        navigate("/dashboard");
+      } else
+        {
+        setPasswordValidationError(response.message);
       }
-
-      setSnackbarMessage(
-        `Registrierung erfolgreich! Du wirst zum Login weitergeleitet und kannst dich mit ${data.username} anmelden`
-      );
-      setSnackbarOpen(true);
     } catch (err) {
       console.error("Fehler beim Aufruf:", err);
+      setSnackbarMessage("Fehler beim Login. Bitte überprüfe deine Eingaben.");
+      setSnackbarOpen(true);
     }
   }
 
   return (
-    <div className="registration-form">
-      <form onSubmit={handleSubmit}>
-        <TextField
-          error={emailValidationError !== ""}
-          helperText={emailValidationError}
-          style={{ width: "200px", margin: "5px" }}
-          type="email"
-          label="Email"
-          variant="outlined"
-          required
-          onChange={(e) => {
-            setEmail(e.target.value);
-            setEmailValidationError("");
-          }}
-        />
-        <TextField
-          error={usernameValidationError !== ""}
-          helperText={usernameValidationError}
-          style={{ width: "200px", margin: "5px" }}
-          type="text"
-          label="Username"
-          variant="outlined"
-          required
-          onChange={(e) => {
-            setUsername(e.target.value);
-            setUsernameValidationError("");
-          }}
-        />
-        <TextField
-          error={passwordValidationError !== ""}
-          style={{ width: "200px", margin: "5px" }}
-          type="password"
-          label="Passwort"
-          variant="outlined"
-          required
-          onChange={(e) => {
-            setPassword(e.target.value);
-            setPasswordValidationError("");
-          }}
-        />
-        
-        <br />
-        <Button
-          sx={{ margin: "5px" }}
-          component={Link}
-          to="/"
-          variant="outlined"
-          color="primary"
+    <form onSubmit={handleSubmit}>
+      <TextField
+        label="Username"
+        variant="outlined"
+        value={username}
+        onChange={(e) => setUsername(e.target.value)}
+        error={userNameValidationError !== ""}
+        helperText={userNameValidationError}
+        margin="normal"
+      />
+      <TextField
+        label="Passwort"
+        variant="outlined"
+        type="password"
+        value={masterpassword}
+        onChange={(e) => setMasterpassword(e.target.value)}
+        error={masterPasswordValidationError !== ""}
+        helperText={masterPasswordValidationError}
+        margin="normal"
+      />
+      <br />
+      <Button
+        sx={{ margin: "5px" }}
+        component={Link}
+        to="/"
+        variant="outlined"
+        color="primary"
         >
-          Zurück
-        </Button>
-        <Button
-          sx={{ margin: "5px" }}
-          variant="contained"
-          color="primary"
-          type="submit"
-        >
-          Einloggen
-        </Button>
-      </form>
+        Zurück
+      </Button>
+      <Button 
+        sx={{ margin: "5px" }} 
+        type="submit"
+        variant="contained" 
+        color="primary">
+        Login
+      </Button>
       <Snackbar
         open={snackbarOpen}
-        autoHideDuration={3000}
-        onClose={() => {
-          setSnackbarOpen(false);
-          navigate("/dashboard");
-        }}
+        autoHideDuration={6000}
+        onClose={() => setSnackbarOpen(false)}
         message={snackbarMessage}
       />
-    </div>
+    </form>
   );
 }
 
-export default RegistrationForm;
+export default LoginForm;
