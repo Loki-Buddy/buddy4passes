dce50cf7c7ce475c7d2630275061c9e9e6de1073
Loki-Buddy <jm.conring@live.de>
2025-11-14 12:49:39 +0100

    first commit für den merge

---
+++ buddy4passes/src/pages/MasterEdit.jsx
@@ -0,0 +1,4 @@
+import { Header } from "../components/Header";
+import { Footer } from "../components/Footer";
+import { invoke } from "@tauri-apps/api/core";
+import { useEffect, useState } from "react";
