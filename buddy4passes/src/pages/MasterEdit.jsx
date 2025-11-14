2bbb429c4cec7818213735a089166bf0a176735e
Loki-Buddy <jm.conring@live.de>
2025-11-14 13:06:29 +0100

    test

--- buddy4passes/src/pages/MasterEdit.jsx
+++ buddy4passes/src/pages/MasterEdit.jsx
@@ -2,3 +2,13 @@ import { Header } from "../components/Header";
 import { Footer } from "../components/Footer";
 import { invoke } from "@tauri-apps/api/core";
 import { useEffect, useState } from "react";
+
+export function MasterEdit() {
+    return (
+        <main className="MasterEdit">
+            <Header />
+            <h2>MasterEdit</h2>
+            <Footer />
+        </main>
+    );
+}
