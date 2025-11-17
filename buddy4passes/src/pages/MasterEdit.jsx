0b75ddde81f8d1080af7e928d4a752d10fc97b8b
Loki-Buddy <jm.conring@live.de>
2025-11-17 13:56:45 +0100

    vorbereitung für merge mit der neuesten Main

--- buddy4passes/src/pages/MasterEdit.jsx
+++ buddy4passes/src/pages/MasterEdit.jsx
@@ -2,6 +2,8 @@ import { Header } from "../components/Header";
 import { Footer } from "../components/Footer";
 import { invoke } from "@tauri-apps/api/core";
 import EditIcon from "@mui/icons-material/Edit";
+import InputAdornment from "@mui/material/InputAdornment";
+import { IconButton } from "@mui/material";
 import VisibilityIcon from "@mui/icons-material/Visibility";
 import VisibilityOffIcon from "@mui/icons-material/VisibilityOff";
 import Box from "@mui/material/Box";
@@ -12,7 +14,7 @@ import Snackbar from "@mui/material/Snackbar";
 import { useState, useEffect } from "react";
 import "./../styles/MasterEdit.css";
 
-export function MasterEdit( { onSubmit } ) {
+export function MasterEdit({ onSubmit }) {
     const [email, setEmail] = useState("");
     const [username, setUsername] = useState("");
     const [oldusername, setOldUsername] = useState("");
@@ -21,6 +23,12 @@ export function MasterEdit( { onSubmit } ) {
     const [oldpassword, setOldPassword] = useState("");
     const [confirmPassword, setConfirmPassword] = useState("");
 
+    const [editEmail, setEditEmail] = useState(false);
+    const [editUsername, setEditUsername] = useState(false);
+    const [showPassword, setShowPassword] = useState(false);
+
+    const [changePassword, setChangePassword] = useState(false);
+
     const [emailValidationError, setEmailValidationError] = useState("");
     const [usernameValidationError, setUsernameValidationError] = useState("");
     const [passwordValidationError, setPasswordValidationError] = useState("");
@@ -28,9 +36,9 @@ export function MasterEdit( { onSubmit } ) {
     async function fetchData() {
         try {
             const response = await invoke("fetch_data");
-            
-            console.log("MasterCreds geladen:", response);
-            
+
+            //console.log("MasterCreds geladen:", response);
+
             setEmail(response.user_email);
             setUsername(response.user_name);
             setOldUsername(response.user_name);
@@ -43,6 +51,7 @@ export function MasterEdit( { onSubmit } ) {
         fetchData();
     }, []);
 
+    const iconStyle = { cursor: "pointer", "&:hover": { color: "#1976d2" } };
     async function handleSubmit(e) {
         e.preventDefault();
 
@@ -52,30 +61,39 @@ export function MasterEdit( { onSubmit } ) {
         } else {
             setPasswordValidationError("");
         }
-        
+
         let data = {
             new_user_email: email,
-            new_user_name: username,
             new_master_password: newpassword,
             old_master_password: oldpassword,
             confirm_new_master_Password: confirmPassword,
         };
 
+        if (username !== oldusername) {
+            data.new_user_name = username;
+        }
+        console.log(data, username);
         const response = await invoke("change_master_creds", {
             data,
             username: oldusername,
         });
 
-        switch (response.message) {
-            case "Dieser Benutzername wird bereits von einem anderen User verwendet.":
-            setUsernameValidationError("Nutzername wird bereits verwendet");
-            return;
-            case "Diese E-Mail wird bereits von einem anderen User verwendet.":
-            setEmailValidationError("E-Mail wird bereits verwendet");
-            return;
-            default:
-            break;
+        if (response.message){
+            switch (response.message) {
+                case "Dieser Benutzername wird bereits von einem anderen User verwendet.":
+                    setUsernameValidationError("Nutzername wird bereits verwendet");
+                    return;
+                case "Diese E-Mail wird bereits von einem anderen User verwendet.":
+                    setEmailValidationError("E-Mail wird bereits verwendet");
+                    return;
+                default:
+                    break;
+            }
         }
+        setEditEmail(false);
+        setEditUsername(false);
+        fetchData();
+        console.log("MasterCreds geändert:", response);
     }
 
     return (
@@ -84,6 +102,73 @@ export function MasterEdit( { onSubmit } ) {
             <h2>MasterEdit</h2>
             <div className="master">
                 <form onSubmit={handleSubmit}>
+                    <TextField
+                        label="E-Mail"
+                        type="email"
+                        value={email}
+                        disabled={!editEmail}
+                        onChange={(e) => setEmail(e.target.value)}
+                        InputProps={{
+                            endAdornment: (
+                                <InputAdornment position="end">
+                                    <Tooltip title="Bearbeiten">
+                                        <IconButton
+                                            size="small"
+                                            onClick={() => setEditEmail(true)}
+                                        >
+                                            <EditIcon sx={iconStyle} />
+                                        </IconButton>
+                                    </Tooltip>
+                                </InputAdornment>
+                            ),
+                        }}
+                    />
+                    <TextField
+                        label="Benutzername"
+                        variant="outlined"
+                        type="text"
+                        value={username}
+                        onChange={(e) => {setUsername(e.target.value); console.log("Username:", e.target.value);}}
+                        disabled={!editUsername}
+                        InputProps={{
+                            endAdornment: (
+                                <InputAdornment position="end">
+                                    <Tooltip title="Bearbeiten">
+                                        <IconButton
+                                            size="small"
+                                            onClick={() => setEditUsername(true)}
+                                        >
+                                            <EditIcon sx={iconStyle} />
+                                        </IconButton>
+                                    </Tooltip>
+                                </InputAdornment>
+                            ),
+                        }}
+                    />
+                    <Button onClick={() => setChangePassword(!changePassword)}>Passwort Ändern</Button>
+                    {changePassword && (
+                        <div>
+                            <TextField
+                                label="Altes Passwort"
+                                type="password"
+                                onChange={(e) => setOldPassword(e.target.value)}
+                                required
+                            />
+                            <TextField
+                                label="Neues Passwort"
+                                type="password"
+                                onChange={(e) => setPassword(e.target.value)}
+                                required
+                            />
+                            <TextField
+                                label="Passwort bestätigen"
+                                type="password"
+                                onChange={(e) => setConfirmPassword(e.target.value)}
+                                required
+                            />
+                        </div>
+                    )}
+                    <Button type="submit">Speichern</Button>                    
                 </form>
             </div>
             <Footer />
